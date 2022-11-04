e32360fcdc6632f15e7c87c32d16de296fcb260d
cesmarLopmar <117409755+cesmarLopmar@users.noreply.github.com>
2022-11-04 12:38:13 -0600

    cambios en fuction calcular IR

--- js/excersices.js
+++ js/excersices.js
@@ -84,5 +84,18 @@ function CalcularOperacion(){
     </form>
 </div>
 
-
+function CalcularIR(SalBruto){
+    let SalBrutoAnual = SalBruto * 12;
+    let IRAnual = 0;
+    if(SalBrutoAnual <= 100000) IRAnual = 0;
+    else if(SalBrutoAnual > 100000 && SalBrutoAnual <= 200000)
+        IRAnual = (SalBrutoAnual - 100000) * 0.15;
+    else if(SalBrutoAnual > 200000 && SalBrutoAnual <= 350000)
+        IRAnual = ((SalBrutoAnual - 200000) * 0.20) + 15000;
+    else if(SalBrutoAnual > 350000 && SalBrutoAnual <= 500000)
+        IRAnual = ((SalBrutoAnual - 350000) * 0.25) + 45000;
+    else
+        IRAnual = ((SalBrutoAnual - 500000) * 0.30) + 82500;
+        return IRAnual / 12;
+}
 
